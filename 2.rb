2b1b45f7be4bb590dd2cd703815d6ed6643189fc
Alex Brook <alex@alexbrook.xyz>
2021-12-04 11:40:13 +0000

    day 2: part 2

--- 2.rb
+++ 2.rb
@@ -1,22 +1,51 @@
 require 'minitest/autorun'
 
 module Submarine
-  def execute_orders(filename)
+  def move(filename)
     move = File
            .readlines(filename)
            .group_by { |order| order.split[0] } # Group by direction
            .transform_keys(&:to_sym)
-           .transform_values { |direction| direction.sum { |order| order.split[1].to_i } }
+           .transform_values { |orders| orders.map { |order| order.split[1].to_i }.sum }
 
     move[:forward] * (move[:down] - move[:up])
   end
+
+  def move_with_aim(filename)
+    (_aim, depth, dist) =
+      File
+      .readlines(filename)
+      .map(&:split)
+      .map { |order| [order.first.to_sym, order.last.to_i] }
+      .reduce([0, 0, 0]) do |(aim, depth, dist), (direction, amount)|
+        case direction
+        when :forward
+          [aim, depth + (aim * amount), dist + amount]
+        when :up
+          [aim - amount, depth, dist]
+        when :down
+          [aim + amount, depth, dist]
+        end
+      end
+
+    dist * depth
+  end
+
+  private
+
+  def grouped_orders(filename); end
 end
 
 class SubmarineTest < Minitest::Test
   include Submarine
 
   def test_p1
-    assert_equal 150, execute_orders('2/example.txt')
-    assert_equal 2_322_630, execute_orders('2/input.txt')
+    assert_equal 150, move('2/example.txt')
+    assert_equal 2_322_630, move('2/input.txt')
+  end
+
+  def test_p2
+    assert_equal 900, move_with_aim('2/example.txt')
+    assert_equal 2_105_273_490, move_with_aim('2/input.txt')
   end
 end
