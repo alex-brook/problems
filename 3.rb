5af2b0ee6f56943341a054faf21fb0f718280f01
Alex Brook <alex@alexbrook.xyz>
2021-12-07 00:28:26 +0000

    rewrite day 3: bit manipulation -> string based

--- 3.rb
+++ 3.rb
@@ -1,30 +1,58 @@
 require 'minitest/autorun'
 
 module GammaEpsilon
-  def power_consumption(filename)
-    lines = File.readlines(filename)
-    word_length = lines.first.length - 1
-    nums = lines.map { |num| num.to_i(2) }
-
-    rate(nums, word_length, :gamma) * rate(nums, word_length, :epsilon)
+  def read(filename)
+    File
+      .readlines(filename)
+      .map(&:strip)
   end
 
-  # private
+  def power_consumption(filename)
+    gamma = read(filename)
+            .then { |nums| (0...nums.first.length).map { |pos| mcb(nums, pos) } }
 
-  def rate(nums, word_length, kind = :gamma, pos = 0, res = 0)
-    return res if pos >= word_length
+    epsilon = gamma.map { |x| x == '1' ? '0' : '1' }
 
-    # Most common bit at position pos
-    mcb = nums.sum { |num| num >> pos & 1 } > (nums.length / 2)
-    mcb = !mcb if kind == :epsilon
+    [gamma, epsilon]
+      .map(&:join)
+      .map { |x| x.to_i(2) }
+      .reduce(:*)
+  end
 
-    rate(
-      nums,
-      word_length,
-      kind,
-      pos + 1,
-      res + 2**pos * (mcb ? 1 : 0)
-    )
+  def life_support_rating(filename)
+    nums = read(filename)
+
+    oxygen = filter_until_one(nums) { |nums, index| mcb(nums, index) }.to_i(2)
+    co2 = filter_until_one(nums) { |nums, index| lcb(nums, index) }.to_i(2)
+
+    oxygen * co2
+  end
+
+  private
+
+  def filter_until_one(nums, index = 0, &blk)
+    return nums.first if nums.one?
+
+    filter_until_one(nums.filter { |num| num[index] == blk.call(nums, index) }, index + 1, &blk)
+  end
+
+  def until_one(nums, index = 0, &blk)
+    return nums.first if nums.one?
+
+    until_one(blk.call(nums, index), index + 1, &blk)
+  end
+
+  def mcb(nums, pos)
+    nums
+      .map { |x| x[pos] }
+      .tally
+      .to_a
+      .max_by { |(bit, count)| [count, bit] }
+      .first
+  end
+
+  def lcb(nums, pos)
+    mcb(nums, pos) == '1' ? '0' : '1'
   end
 end
 
@@ -33,6 +61,11 @@ class GammaEpsilonTest < Minitest::Test
 
   def test_p1
     assert_equal 198, power_consumption('3/example.txt')
-    assert_equal 3813416, power_consumption('3/input.txt')
+    assert_equal 3_813_416, power_consumption('3/input.txt')
+  end
+
+  def test_p2
+    assert_equal 230, life_support_rating('3/example.txt')
+    assert_equal 0, life_support_rating('3/input.txt')
   end
 end
