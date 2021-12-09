6e06c24a8708e16a2ce7dfaf06d21ac6e7737759
Alex Brook <alex@alexbrook.xyz>
2021-12-09 22:01:18 +0000

    day 6: rewrite

--- 6.rb
+++ 6.rb
@@ -1,49 +1,56 @@
 require 'minitest/autorun'
 
 module LaternFish
-  def simulate(state, days)
-    step(state.map { |value| Counter.new(value) }, days).length
-  end
+  def simulate(filename, days)
+    counter = Counter.new(filename)
 
-  def step(collection, n = 1)
-    return collection if n.zero?
+    days.times do
+      counter.step
+    end
 
-    step(collection.map(&:dec).flatten, n - 1)
+    counter.total
   end
 
   class Counter
-    INITIAL = 6
-    NEW_INITIAL = INITIAL + 2
+    def initialize(filename)
+      start = File
+              .readlines(filename)
+              .first
+              .strip
+              .split(',')
+              .map(&:to_i)
+              .tally
 
-    attr_accessor :value
+      @state = (0..8).to_h { |n| [n, [0, 0]] }
 
-    def initialize(value)
-      @value = value
-    end
-
-    def dec
-      @value -= 1
-
-      if @value.negative?
-        @value = INITIAL
-
-        return [self, Counter.new(NEW_INITIAL)]
+      start.each do |k, v|
+        @state[k][1] += v
       end
-
-      self
     end
 
-    def inspect
-      "~#{@value}"
+    def step
+      new_fish = @state[0].sum
+      @state = @state.map { |k, _v| [k, @state[k + 1] || [new_fish, 0]] }.to_h
+      @state[6][1] += new_fish
+    end
+
+    def total
+      @state.values.flatten.sum
     end
   end
 end
 
-class LanternFish < Minitest::Test
+class LanternFishTest < Minitest::Test
   include LaternFish
 
+  def test_p1
+    assert_equal 26, simulate('6/example.txt', 18)
+    assert_equal 5934, simulate('6/example.txt', 80)
+    assert_equal 26_984_457_539, simulate('6/example.txt', 256)
+    assert_equal 351_092, simulate('6/input.txt', 80)
+  end
+
   def test_p2
-    assert_equal 26, simulate([3, 4, 3, 1, 2], 18)
-    assert_equal 5934, simulate([3, 4, 3, 1, 2], 80)
+    assert_equal 1_595_330_616_005, simulate('6/input.txt', 256)
   end
 end
