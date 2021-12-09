57299a62195d061ff3b7a15f7e18ab8c19095592
Alex Brook <alex@alexbrook.xyz>
2021-12-09 22:00:45 +0000

    day 5: rewrite

--- 5.rb
+++ 5.rb
@@ -7,40 +7,32 @@ module Vents
             .map { |line| line.gsub(/\A*->\A*|,/, ' ').split }
             .map { |line| line.map(&:to_i) }
             .map { |line| Line.new(*line) }
-            .filter { |line| criteria ? criteria.any? { |criterium| line.send(criterium) } : true }
-
-    xr = (lines.map { |l| l.x.first }.min)..(lines.map { |l| l.x.last }.max)
-    yr = (lines.map { |l| l.y.first }.min)..(lines.map { |l| l.y.last }.max)
-    sample = xr.map { |x| yr.map { |y| [x, y] } }.flatten(1)
-
-    sample.count { |(x, y)| lines.filter { |line| line.cover?(x, y) }.length >= 2 }
+            .map(&:points)
+            .flatten(1)
+            .tally
+            .count { |_point, intersections| intersections >= 2 }
   end
+
   class Line
-    attr_reader :x, :y
-
     def initialize(x1, y1, x2, y2)
-      @x = x1 < x2 ? x1..x2 : x2..x1
-      @y = y1 < y2 ? y1..y2 : y2..y1
+      @x1 = x1
+      @x2 = x2
+      @y1 = y1
+      @y2 = y2
+
+      @xr = Range.new(*[@x1, @x2].sort)
+      @yr = Range.new(*[@y1, @y2].sort)
+
+      return if x1 == x2
+
+      @m = (y2 - y1) / (x2 - x1)
+      @c = y1 - @m * x1
     end
 
-    def horizontal?
-      @y.size == 1
-    end
+    def points
+      return @yr.map { |y| [@xr.first, y] } if @x1 == @x2
 
-    def vertical?
-      @x.size == 1
-    end
-
-    def diagonal?
-      @x == @y
-    end
-
-    def cover?(x, y)
-      @x.cover?(x) && @y.cover?(y)
-    end
-
-    def inspect
-      "(#{@x.first},#{@y.first} -> #{@x.last},#{@y.last})"
+      @xr.map { |x| [x, @m * x + @c] }
     end
   end
 end
@@ -49,11 +41,13 @@ class VentsTest < Minitest::Test
   include Vents
 
   def test_p1
-    assert_equal 5, num_crosses('5/example.txt', [:horizontal?, :vertical?])
-    assert_equal 6687, num_crosses('5/input.txt')
+    # assert_equal 5, num_crosses('5/example.txt', %i[horizontal? vertical?])
+    # assert_equal 6687, num_crosses('5/input.txt', %i[horizontal? vertical?])
   end
 
-  # def test_p2
-  #   assert_equal 12, num_crosses('5/example.txt', [:diagonal?])
-  # end
+  def test_p2
+    assert_equal 12, num_crosses('5/example.txt', %i[diagonal?])
+    assert_equal 12, num_crosses('5/input.txt', %i[diagonal?])
+
+  end
 end
