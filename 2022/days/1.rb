f7adb1141101ea0ff6b79a16af18061018851609
Alex Brook <43730469+alex-brook@users.noreply.github.com>
2022-12-01 19:12:03 +0000

    2022: day one

---
+++ 2022/days/1.rb
@@ -0,0 +1,20 @@
+require_relative "../spec_helper.rb"
+
+class DayOne < Day
+  def solve(filename, n=1)
+    File
+      .foreach(filename)
+      .chunk_while { _1 != "\n" }
+      .map { _1.map(&:to_i).sum }
+      .max(n)
+      .sum
+  end
+
+  # part one
+  it { expect(solve("days/1_example.txt")).to eq 24000 }
+  it { expect(solve("days/1_input.txt")).to eq 72602 }
+
+  # part two
+  it { expect(solve("days/1_example.txt", 3)).to eq 45000 }
+  it { expect(solve("days/1_input.txt", 3)).to eq 207410 }
+end
