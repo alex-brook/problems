11d8e8d0d45ca1fb97d71c1b985dee8a7b4fb81d
Alex Brook <43730469+alex-brook@users.noreply.github.com>
2023-11-18 17:25:51 +0000

    05 permutations
    permutations

---
+++ cses/05_permutations.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  if (n == 2 || n == 3) {
+    cout << "NO SOLUTION";
+  } else {
+    for (int i = 2; i <= n; i += 2) cout << i << " ";
+    for (int i = 1; i <= n; i += 2) cout << i << " ";
+  }
+}
