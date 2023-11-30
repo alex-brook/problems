6925f4549fc8de669f983fc302d0b54cd264fbd1
Alex Brook <43730469+alex-brook@users.noreply.github.com>
2023-11-30 17:50:41 +0000

    aoc 2023 template

---
+++ advent-of-code/2023/days/foo.rb
@@ -0,0 +1,5 @@
+require_relative "../spec_helper"
+
+class DayOne < Day
+  it { expect(1).to eq(1) }
+end
---
+++ advent-of-code/2023/spec_helper.rb
@@ -0,0 +1,6 @@
+require "minitest/autorun"
+require "minitest/spec"
+require "rspec/expectations/minitest_integration"
+
+class Day < Minitest::Spec
+end
